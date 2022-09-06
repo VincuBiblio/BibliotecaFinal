2d81e8abd4c65b5b7abc296f356fe246e6e872c3
Marco Santander <marcosantander989@gmail.com>
2022-09-05 22:09:02 -0500

    push

--- Biblioteca/src/main/java/com/Biblioteca/Repository/Libros/PrestamoLibrosRepository.java
+++ Biblioteca/src/main/java/com/Biblioteca/Repository/Libros/PrestamoLibrosRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 
 public interface PrestamoLibrosRepository extends JpaRepository<PrestamoLibros, Long> {
 
-    Optional<PrestamoLibros> findByCodigoLibro(String codigo);
+    Optional<PrestamoLibros> findByCodigoLibroLikeIgnoreCase(String codigo);
 
     List<PrestamoLibros> findAllByEstado(Boolean estado);
 }
