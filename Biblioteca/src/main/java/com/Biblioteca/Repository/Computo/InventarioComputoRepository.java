92d70f1a425277b135607d79d5fe4157d6ecc61d
fsdominguez12 <franklin.dominguez.est@tecazuay.edu.ec>
2022-09-20 11:59:39 -0500

    Ordenar computadoras estado ASC

--- Biblioteca/src/main/java/com/Biblioteca/Repository/Computo/InventarioComputoRepository.java
+++ Biblioteca/src/main/java/com/Biblioteca/Repository/Computo/InventarioComputoRepository.java
@@ -2,6 +2,7 @@ package com.Biblioteca.Repository.Computo;
 
 import com.Biblioteca.Models.Servicio.CentroComputo.InventarioComputo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,5 +11,9 @@ public interface InventarioComputoRepository extends JpaRepository<InventarioCom
 
     Optional<InventarioComputo> findByNumero(Long numero);
 
+    @Query(value = "SELECT *\n" +
+            "FROM inventario_computo\n" +
+            "where estado = 'false' and estado_prestamo = 'false'\n" +
+            "ORDER BY numero ASC ", nativeQuery = true)
     List<InventarioComputo> findAllByEstado(Boolean estado);
 }
